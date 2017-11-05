760770edeeeafdc38e0245b7c26f174a584fafb4
Barend Erasmus <developersworkspace@gmail.com>
2017-11-05 17:54:57 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
-# Dijkstra's algorithm
-
-Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph, which may represent, for example, road networks. It was conceived by computer scientist Edsger W. Dijkstra in 1956 and published three years later.
-
-
-```javascript
 const source = 'A';
 const destination = 'C';
 
@@ -98,4 +92,3 @@ function findSmallestUnvisitedVertex(table, unvisitedVertex) {
 
     return vertex;
 }
-```
